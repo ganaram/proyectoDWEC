5613c0ee0b1e7ef3abb7888f4fa0b66da34cb497
Guillermo Cano Rama <32271984+ganaram@users.noreply.github.com>
2018-11-04 20:32:59 +0100

    Create clases.js
    Constructor de clase Usuario.
    
    Atributos username, passwd, email y DNI.

---
+++ clases.js
@@ -0,0 +1,34 @@
+class Usuario{
+    constructor(username, passwd, email, nombreReal){
+        this.username = username;
+        this.passwd = passwd;
+        this.email = email;
+        this.DNI = DNI;
+    }
+
+    set username(username){
+        this._username = username;
+    }
+    set passwd(passwd){
+        this._passwd = passwd;
+    }
+    set correo(correo){
+        this._email = email;
+    }
+    set DNI(DNI){
+        this._DNI = DNI;
+    }
+
+    get username(){
+        return this._username;
+    }
+    get passwd(){
+        return this._passwd;
+    }
+    get email(){
+        return this._email;
+    }
+    get DNI(){
+        return this._DNI;
+    }
+}
